85dbefcfb38ec0ac4cd482bf0ea7786925f5f1b0
srubens <rubensfilipe6@gmail.com>
2019-08-11 01:15:02 -0300

    validando dados no campo

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -81,8 +81,15 @@ class UserController {
 	getValues(){
 
 		let user = {};
+		let isValid = true;
 
 		[...this.formEl.elements].forEach(function (field, index) {
+			
+			if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
+				field.parentElement.classList.add(['has-error']);
+				isValid = false;
+			}
+
 			if (field.name == "gender") {
 				if (field.checked) {
 					user[field.name] = field.value;
@@ -94,6 +101,10 @@ class UserController {
 			}
 
 		});
+
+		if(!isValid){
+			return false;
+		}
 
 		return new User(user.name,user.gender,user.birth,user.country,user.email,user.password,user.photo,user.admin, user.data);
 
@@ -107,7 +118,7 @@ class UserController {
                     <td>${dataUser.name}</td>
                     <td>${dataUser.email}</td>
                     <td>${(dataUser.admin) ? 'sim' : 'não'}</td>
-                    <td>${dataUser.register}</td>
+                    <td>${Utils.dataFormat(dataUser.register)}</td>
                     <td>
                       <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                       <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
